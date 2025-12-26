fe0bc73b0dff59cd9fe1be6fb70532854e7ecb9e
ngocly261 <luongnguyetvien@mail.com>
2025-12-26 08:33:05 +0700

    Lưu lại các thay đổi

--- dangnhap.py
+++ dangnhap.py
@@ -1,4 +1,3 @@
-pip install flask flask-sqlalchemy flask-login
 from flask import Flask, render_template, redirect, url_for, request, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
